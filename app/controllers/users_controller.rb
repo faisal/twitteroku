4be975b0ef31b80d60d343ca9de45621852a0e2b
Faisal N. Jawdat <faisal@faisal.com>
2010-06-30 13:49:58 -0700

    View our own timeline.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def info
     @result = Twitter.user(params[:name])
   end
+
+  def timeline
+    @timeline = twitter_client.friends_timeline
+  end
 end
